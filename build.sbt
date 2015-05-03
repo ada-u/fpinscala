a6947ab85b79575745653f66df0ec7e05d76d281
YutaAdachi <ada-u@ada-u.com>
2015-05-03 16:56:37 +0900

    Implement MyOption

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "fpinscala"
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+    
---
+++ src/main/scala/fpinscala/option/MyOption.scala
@@ -0,0 +1,52 @@
+package fpinscala.option
+
+sealed trait MyOption[+A]  {
+
+  def get: A
+
+  def isEmpty: Boolean
+
+  def map[B](f: A => B): MyOption[B] =
+    if (isEmpty) MyNone else MySome(f(get))
+
+  def flatMap[B](f: A => MyOption[B]): MyOption[B] =
+    if (isEmpty) MyNone else f(get)
+
+  def getOrElse[B >: A](elseValue: B): B =
+    if (isEmpty) elseValue else get
+
+
+  def filter(f: A => Boolean): MyOption[A] =
+    flatMap(a => if (f(a)) MySome(a) else MyNone)
+
+  def orElse[B >: A](elseValue: => MyOption[B]): MyOption[B] =
+    map(MySome(_)).getOrElse(elseValue)
+
+  //def toList: MyList[A] =
+  //  if (isEmpty) MyNil else MyList(get)
+
+}
+
+case object MyNone extends MyOption[Nothing] {
+
+  def get = throw new NoSuchElementException("None.get")
+
+  def isEmpty = true
+
+}
+
+case class MySome[+A](x: A) extends MyOption[A] {
+
+  def get = x
+
+  def isEmpty = false
+}
+
+object MyOption {
+
+  def empty[A]: MyOption[A] = MyNone
+
+  def apply[A](x: A): MyOption[A] =
+    if (x == null) MyNone else MySome(x)
+
+}
