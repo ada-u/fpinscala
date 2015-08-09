b420525c4a90f7933da1a54e6000daa4f320fdf3
YutaAdachi <ada-u@ada-u.com>
2015-08-09 13:38:02 +0900

    Implement consoleToState

--- src/main/scala/fpinscala/io/console/ConsoleApplicationSample.scala
+++ src/main/scala/fpinscala/io/console/ConsoleApplicationSample.scala
@@ -19,6 +19,10 @@ object ConsoleApplicationSample {
     def apply[A](a: Console[A]): ConsoleReader[A] = a.toReader
   }
 
+  val consoleToState: (Console ~> ConsoleState) = new (Console ~> ConsoleState) {
+    def apply[A](a: Console[A]): ConsoleState[A] = a.toState
+  }
+
   def runConsoleFunction0[A](a: Free[Console, A]): () => A =
     runFree[Console, Function0, A](a)(consoleToFunction0)
 
@@ -28,9 +32,19 @@ object ConsoleApplicationSample {
   def runConsoleReader[A](a: Free[Console, A]): ConsoleReader[A] =
     runFree[Console, ConsoleReader, A](a)(consoleToReader)
 
+  def runConsoleState[A](a: Free[Console, A]): ConsoleState[A] =
+    runFree[Console, ConsoleState, A](a)(consoleToState)
+
   def runConsole[A](a: Free[Console, A]): A = {
     runTrampoline(translate(a)(consoleToFunction0))
   }
+
+  val app: Free[Console, Option[String]] = for {
+    _ <- Console.printLn("I can only interact with the console")
+    ln <- Console.readLn
+  } yield ln
+
+  // ---
 
   implicit val function0Monad: Monad[Function0] = new Monad[Function0] {
 
@@ -48,10 +62,5 @@ object ConsoleApplicationSample {
     def unit[A](a: => A): Par[A] = Par.unit(a)
 
   }
-
-  val app: Free[Console, Option[String]] = for {
-    _ <- Console.printLn("I can only interact with the console")
-    ln <- Console.readLn
-  } yield ln
   
 }
